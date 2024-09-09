732b30142414243bb6c01b20f2b56ed45a79cf51
kevincode06 <113902012+kevincode06@users.noreply.github.com>
2024-09-10 01:20:33 +0200

    i just added recipe list.js

---
+++ src/components/RecipeList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/RecipeStyles.css';
+
+const RecipeList = ({ recipes }) => {
+  return (
+    <div className="recipe-list">
+      <h1>Recipe Book</h1> {/* Title at the top */}
+      <div className="recipe-cards">
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className="recipe-card">
+            <img src={recipe.image} alt={recipe.title} />
+            <h2>{recipe.title}</h2>
+            <p>{recipe.description}</p>
+            <Link to={`/recipe/${recipe.id}`}>View Recipe</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecipeList;

645902888135ca17f1da2feece37918a5e767b76
kevincode06 <113902012+kevincode06@users.noreply.github.com>
2024-09-10 01:18:44 +0200

    i just add the Recipe detail.js

---
+++ src/components/RecipeDetail.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import '../styles/RecipeStyles.css'; // Corrected path
+
+const RecipeDetail = ({ recipes }) => {
+  const { id } = useParams();
+  const recipe = recipes.find((recipe) => recipe.id === parseInt(id));
+
+  if (!recipe) {
+    return <div>Recipe not found!</div>;
+  }
+
+  return (
+    <div className="recipe-detail">
+      <h1 className='detail-tittle'>{recipe.title}</h1>
+      <img src={recipe.image} alt={recipe.title} />
+      <h3>Ingredients:</h3>
+      <ul>
+        {recipe.ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+      <h3>Steps:</h3>
+      <ol>
+        {recipe.steps.map((step, index) => (
+          <li key={index}>{step}</li>
+        ))}
+      </ol>
+      <Link to="/">Back to Recipes</Link>
+    </div>
+  );
+};
+
+export default RecipeDetail;
